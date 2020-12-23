9012b6ea2d0e0f823339c5f0ae93f17451abc355
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2020-12-22 19:29:48 -0500

    down to 72291338:22:04

--- 684.py
+++ 684.py
@@ -1,14 +1,24 @@
 from otqdm import otqdm
 
+seen = [1,10]
+def mod10(nines):
+    while nines >= len(seen):
+        seen.append((seen[-1]*10)% 1000000007)
+    return seen[nines]
+
 
 def s(n):
     """smallest number with a digit sum of n"""
     nines, remainder = divmod(n, 9)
-    return int(str(remainder) + "9"*nines) % 1000000007
+    return ((remainder+1) * mod10(nines) - 1) % 1000000007
 
 
 def S(k):
-    return sum([s(n) % 1000000007 for n in range(1,k+1)]) % 1000000007
+    tot = 0
+    for n in range(1, k + 1):
+        tot += s(n)
+        tot %= 1000000007
+    return tot
 
 
 fib = [0,1,1]
